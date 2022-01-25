5118bd3372a646ffa0fea56719de1125f1b929df
pixelzee3 <bpixelzee@gmail.com>
2022-01-25 23:15:24 +0700

    Change tab size to 2.

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import Navbar from './Navbar';
 import Home from './Home';
 
 function App() {
-    const num = Math.floor(Math.random() * 10);
+  const num = Math.floor(Math.random() * 10);
 
-    return (
-        <div className="App">
-            <Navbar />
-            <div className="content">
-                <Home />
-            </div>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Home />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/Home.jsx
+++ src/Home.jsx
@@ -2,46 +2,48 @@ import BlogList from './BlogList';
 import { useState } from 'react';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {
-            title: 'My new website',
-            body: 'lorem ipsum...',
-            author: 'mario',
-            id: 1,
-        },
-        {
-            title: 'Welcome party!',
-            body: 'lorem ipsum...',
-            author: 'yoshi',
-            id: 2,
-        },
-        {
-            title: 'Web dev top tips',
-            body: 'lorem ipsum...',
-            author: 'mario',
-            id: 3,
-        },
-    ]);
+  const [blogs, setBlogs] = useState([
+    {
+      title: 'My new website',
+      body: 'lorem ipsum...',
+      author: 'mario',
+      id: 1,
+    },
+    {
+      title: 'Welcome party!',
+      body: 'lorem ipsum...',
+      author: 'yoshi',
+      id: 2,
+    },
+    {
+      title: 'Web dev top tips',
+      body: 'lorem ipsum...',
+      author: 'mario',
+      id: 3,
+    },
+  ]);
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    };
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
 
-    return (
-        <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
-            <BlogList
-                blogs={blogs.filter((blog) =>
-                    blog.author === 'mario'
-                )}
-                title="Mario's blogs!"
-                handleDelete={handleDelete}
-            />
-            {/* Make a BlogList component that only shows blogs written by yoshi */}
-            <BlogList blogs={blogs.filter((blog) => blog.author === 'yoshi')} title="Yoshi's blogs!" handleDelete={handleDelete}/>
-        </div>
-    );
+  return (
+    <div className="home">
+      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+      <BlogList
+        blogs={blogs.filter((blog) => blog.author === 'mario')}
+        title="Mario's blogs!"
+        handleDelete={handleDelete}
+      />
+      {/* Make a BlogList component that only shows blogs written by yoshi */}
+      <BlogList
+        blogs={blogs.filter((blog) => blog.author === 'yoshi')}
+        title="Yoshi's blogs!"
+        handleDelete={handleDelete}
+      />
+    </div>
+  );
 };
 
 export default Home;
