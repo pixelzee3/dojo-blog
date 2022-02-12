724a2b16a98a7e9a0c933ca35f1118a8af1073a3
pixelzee3 <bpixelzee@gmail.com>
2022-02-12 16:47:03 +0700

    Display 'loading' when fetching data.

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,8 +3,7 @@ import { useState, useEffect } from 'react';
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
-
-  const [name, setName] = useState('mario');
+  const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
@@ -12,12 +11,14 @@ const Home = () => {
         return res.json();
       })
       .then((data) => {
+        setIsPending(false);
         setBlogs(data);
       });
   }, []);
 
   return (
     <div className="home">
+      {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
   );
