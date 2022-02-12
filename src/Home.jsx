6e24e7e2b55cac3ba83d082e88b2b0a3b9ce2e29
pixelzee3 <bpixelzee@gmail.com>
2022-02-12 16:41:17 +0700

    Put data in a database and fetched it using React.

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,54 +2,23 @@ import BlogList from './BlogList';
 import { useState, useEffect } from 'react';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    {
-      title: 'My new website',
-      body: 'lorem ipsum...',
-      author: 'mario',
-      id: 1,
-    },
-    {
-      title: 'Welcome party!',
-      body: 'lorem ipsum...',
-      author: 'yoshi',
-      id: 2,
-    },
-    {
-      title: 'Web dev top tips',
-      body: 'lorem ipsum...',
-      author: 'mario',
-      id: 3,
-    },
-  ]);
+  const [blogs, setBlogs] = useState(null);
 
   const [name, setName] = useState('mario');
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
-
   useEffect(() => {
-    console.log('useEffect ran.');
-    console.log(name);
-  }, [name]);
+    fetch('http://localhost:8000/blogs')
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+      });
+  }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
-      <BlogList
-        blogs={blogs.filter((blog) => blog.author === 'mario')}
-        title="Mario's blogs!"
-        handleDelete={handleDelete}
-      />
-      <BlogList
-        blogs={blogs.filter((blog) => blog.author === 'yoshi')}
-        title="Yoshi's blogs!"
-        handleDelete={handleDelete}
-      />
-      <button onClick={() => setName('luigi')}>Change Name</button>
-      <p>{name}</p>
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
   );
 };
