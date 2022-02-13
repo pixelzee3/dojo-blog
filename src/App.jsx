37c5457a30450fab9013593c4e346fb5425555bf
pixelzee3 <bpixelzee@gmail.com>
2022-02-13 17:32:37 +0700

    Create a 'Create' page using React Router.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Create from './Create';
 
 function App() {
   const num = Math.floor(Math.random() * 10);
 
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
